cdbb6644871790bb976ef648bacb93569d67a8d8
Chris Underwood <cunderwoodmn@gmail.com>
2021-09-28 16:24:01 -0700

    great basic jsx for the register form

--- pages/register.js
+++ pages/register.js
@@ -2,6 +2,47 @@ import React from 'react';
 import Layout from '../components/Layout';
 //import MyApp from _app.js
 
-const Register = () => <Layout>Register Page</Layout>;
+const Register = () => {
+  const registerForm = () => {
+    return (
+      <form>
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Type your name"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Type your email"
+          />{' '}
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Type your password"
+          />
+        </div>
+        <div className="form-group">
+          <button className="btn btn-outline-secondary">Register</button>
+        </div>
+      </form>
+    );
+  };
+
+  return (
+    <Layout>
+      <div className="col-md-6 offset-md-3">
+        <h1>Register</h1>
+        <br />
+        {registerForm()}
+      </div>
+    </Layout>
+  );
+};
 
 export default Register;
