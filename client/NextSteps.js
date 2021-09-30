53b5b721a1dd48600f872cd8bf62e57d8b245420
Chris Underwood <cunderwoodmn@gmail.com>
2021-09-29 21:26:59 -0700

    moved existing files to client folder for frontend. add NextSteps.js folder for notes movung foward

---
+++ client/NextSteps.js
@@ -0,0 +1,26 @@
+/*
+server setup
+
+install npm packages
+
+setup simple mvc pattern
+
+implements JWT based authentication system
+
+before we can save user in database, determine if it exists
+
+check if user's email is valid by sending a verification token as a clickable link to user email
+
+for sending email we will use AWS SES, so we need to signup with AWS. Then ...
+
+Once the user click, we take them to our react app and then take the token from the route
+
+that token consists, of user name, email, and password
+
+Once again send that back to the server
+
+this time we can save the user into the database
+
+This completes user registration.
+
+*/
